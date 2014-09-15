5ab8f33b4a588454c5e25a2be8fdc5ec4c15509e
xopherdeep <heylisten@xtiv.net>
2014-09-15 12:40:09 -0700

    xLang/ Automated Nightly Push

--- xLang.php
+++ xLang.php
@@ -3,7 +3,7 @@
 * @author dev@domain.com
 * @name Language
 * @desc Change the Languauge on your Site.
-* @version v1(0.7)
+* @version v1(0.8)
 * @icon applications-education-language.png 
 * @mini language 
 * @link lang
